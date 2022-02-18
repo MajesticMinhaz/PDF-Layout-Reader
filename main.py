e5545383f41c2c786c1d74459b173512512900be
Md. Minhaz <mdm047767@gmail.com>
2022-02-18 17:06:20 +0600

    Created a class for basic widget. For this Widget class our code will be more readable and more understandable. for create this class we need one buildin module which is called as tkinter. This module is free for use and builin in python.

--- main.py
+++ main.py
@@ -9,4 +9,12 @@
                                 Date        18-02-2022 at 4:56 PM
 ===========================================================================================================
 """
+from tkinter import *
+from tkinter.ttk import *
 
+
+# Basic Widget Class
+class Widget:
+    def __init__(self, master, frame_text: str):
+        self.frame = LabelFrame(master=master, text=frame_text, padding=10)
+        self.frame.grid(row=0, column=0, padx=10, pady=10)
