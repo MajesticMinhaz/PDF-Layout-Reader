a0c404bfc335d841fe547a128b67a4d66756effe
Md. Minhaz <mdm047767@gmail.com>
2022-02-18 16:58:15 +0600

    Author and Project Details here inputed. So that anyone can understand easily about author and project.

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+===========================================================================================================
+                                File        main.py ( Full project code here included )
+                                Author      MD. MINHAZ
+                                Email       mdm047767@gmail.com
+                                Hire Me     https://pph.me/mdminhaz2003/
+                                Repo Link   https://github.com/mdminhaz2003/PDF-Layout-Reader/ (Private Repo)
+                                Location    Dhaka, Bangladesh
+                                Date        18-02-2022 at 4:56 PM
+===========================================================================================================
+"""
+
